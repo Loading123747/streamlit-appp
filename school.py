f8d1f47cb7ad79696686085011812138ddc470c8
Loading123747 <Utuber123wasd@gmail.com>
2024-06-24 14:14:53 -0700

    Create school.py

---
+++ school.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def get_person_name():
+    query_params = st.experimental_get_query_params()
+    return query_params.get("name", ["Person"])[0]
+
+
+st.set_page_config(page_title="Welcome")
+
+PERSON_NAME = get_person_name()
+st.header(f"Welcome, {PERSON_NAME}!", anchor=False)
+st.subheader("Use this link to acces the website and use that browser and everything will be unblocked")
+st.subheader("https://4wt6q5-8080.csb.app/")
