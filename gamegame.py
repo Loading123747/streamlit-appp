d99d39a4d6fba923f75a55b532ab658b746d1478
Loading123747 <Utuber123wasd@gmail.com>
2024-09-21 20:17:07 -0700

    Update gamegame.py

--- gamegame.py
+++ gamegame.py
@@ -7,6 +7,8 @@ PAGES = {
     "Play 2048": "game",
     "Play Powerline": "game2",
     "Play Snake.io": "snake",
+    "Play Wings.io": "Wing",
+    "Play Slither.io": "Slither"
 }
 
 def main():
@@ -27,8 +29,10 @@ def main():
         play_2048()
     elif st.session_state.page == "game2":
         play_powerline()
-    elif st.session_state.page == "snake":
-        play_snake()
+    elif st.session_state.page == "Wing":
+        play_Wing()
+    elif st.session_state.page == "Slither":
+        play_Slither()
 
 def home():
     st.title("Welcome to the Game Portal")
@@ -60,7 +64,7 @@ def play_powerline():
     st.title('Play Powerline')
 
     # URL of the game or webpage you want to embed
-    game_url = 'https://iogames.onl/powerline-io'
+    game_url = 'https://fdmnrn-8080.csb.app/service/hvtr%3A-%2Froueplkng.ko/'
 
     # HTML code to embed the game in an iframe
     iframe_code = f'''
@@ -74,11 +78,11 @@ def play_powerline():
     if st.button('Back to Home'):
         st.session_state.page = 'home'
 
-def play_snake():
-    st.title('Play Snake.io')
+def play_Slither():
+    st.title('Play Slither.io')
 
     # URL of the game or webpage you want to embed
-    game_url = 'https://snake.io/'
+    game_url = 'https://fdmnrn-8080.csb.app/service/hvtrs8%2F-snivhgreaoe%2Cim%2F/'
 
     # HTML code to embed the game in an iframe
     iframe_code = f'''
@@ -91,6 +95,26 @@ def play_snake():
     # Button to go back to the home page
     if st.button('Back to Home'):
         st.session_state.page = 'home'
+
+def play_Wing():
+    st.title('Play Wings.io')
+
+    # URL of the game or webpage you want to embed
+    game_url = 'https://fdmnrn-8080.csb.app/service/hvtr%3A-%2Fuilgq.ko/'
+
+    # HTML code to embed the game in an iframe
+    iframe_code = f'''
+    <iframe src="{game_url}" width="100%" height="800px" style="border:none;"></iframe>
+    '''
+
+    # Render the iframe in the Streamlit app
+    components.html(iframe_code, height=800)
+
+    # Button to go back to the home page
+    if st.button('Back to Home'):
+        st.session_state.page = 'home'
+
+
 
 if __name__ == "__main__":
     main()
